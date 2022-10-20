fa3dc20f9264dfe8d1cde8f65ce61f07ff9e06a1
19307 <wangdiwrg@mail.com>
2022-10-20 21:12:35 +0800

    面

---
+++ springboot_helloworld/src/main/java/com/wd/boot/controller/Test.java
@@ -0,0 +1,11 @@
+package com.wd.boot.controller;
+
+public class Test {
+    public static void main(String args[]){
+        String a = "q";
+        String b = new String(a);
+        System.out.println(a == b);
+    }
+
+
+}
